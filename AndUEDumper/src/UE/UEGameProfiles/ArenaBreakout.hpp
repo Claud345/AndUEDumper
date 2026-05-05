5369f06140c2aeeaa5ff4ff712aa418958d4dd10
Claud345 <g44775368@gmail.com>
2026-05-05 18:26:12 +0800

    Refactor ArenaBreakoutProfile class structure

--- AndUEDumper/src/UE/UEGameProfiles/ArenaBreakout.hpp
+++ AndUEDumper/src/UE/UEGameProfiles/ArenaBreakout.hpp
@@ -1,133 +1,131 @@
-#pragma once
+  #pragma once
 
-#include "../UEGameProfile.hpp"
-using namespace UEMemory;
+  #include "../UEGameProfile.hpp"
+  using namespace UEMemory;
 
-class ArenaBreakoutProfile : public IGameProfile
-{
-public:
-    ArenaBreakoutProfile() = default;
+  class ArenaBreakoutProfile : public IGameProfile
+  {
+  public:
+      ArenaBreakoutProfile() = default;
 
-    bool ArchSupprted() const override
-    {
-        auto e_machine = GetUnrealELF().header().e_machine;
-        return e_machine == EM_AARCH64;
-    }
+      bool ArchSupprted() const override
+      {
+          auto e_machine = GetUnrealELF().header().e_machine;
+          return e_machine == EM_AARCH64;
+      }
 
-    std::string GetAppName() const override
-    {
-        return "Arena Breakout";
-    }
+      std::string GetAppName() const override
+      {
+          return "Arena Breakout";
+      }
 
-    std::vector<std::string> GetAppIDs() const override
-    {
-        return {"com.proximabeta.mf.uamo"};
-    }
+      std::vector<std::string> GetAppIDs() const override
+      {
+          return {"com.proximabeta.mf.uamo"};
+      }
 
-    bool isUsingCasePreservingName() const override
-    {
-        return false;
-    }
+      bool isUsingCasePreservingName() const override
+      {
+          return false;
+      }
 
-    bool IsUsingFNamePool() const override
-    {
-        return true;
-    }
+      bool IsUsingFNamePool() const override
+      {
+          return true;
+      }
 
-    bool isUsingOutlineNumberName() const override
-    {
-        return false;
-    }
+      bool isUsingOutlineNumberName() const override
+      {
+          return false;
+      }
 
-    uintptr_t GetGUObjectArrayPtr() const override
-    {
-        std::vector<std::pair<std::string, int>> idaPatterns = {
-            // CMP W8,#0 / LDR X10,[X10,#off_DDBBA68@PAGEOFF] / CSEL / ASR W11,W9,#0x10
-            {"1F 01 00 71 4A ?? ?? F9 29 B1 88 1A 2B 7D 10 13", -12},
-            // ASR #0x10 / AND #FFFF0000 / SUB / MOV #0x18 / LDR [X10,#ObjObjects] / LDR [X10,#?]
-            {"2B 7D 10 13 29 3D 10 12 08 01 09 4B 09 03 80 52 4A ?? ?? F9 4A ?? ?? F8", -32},
-            // LDR-W8,[X2,#0xC] / ADRP / MOV#FFFF / ADD / CMP / LDR-guobjectarray
-            {"48 0C 40 B9 4A ?? ?? F0 E9 FF 9F 52 09 01 09 0B 1F 01 00 71 4A ?? ?? F9", 4},
-            // MOV #0x18 / LDR ObjObjects / LDR entry
-            {"09 03 80 52 4A ?? ?? F9 4A ?? ?? F8 1F 20 03 D5 08 29 29 9B", -36},
-            // LDR from pointer table with ASR+AND object index resolution
-            {"4A ?? ?? F9 29 B1 88 1A 2B 7D 10 13 29 3D 10 12 08 01 09 4B", -20},
-            // MOV #0x18 (24-byte object stride) + LDR obj ptr + LDR entry
-            {"09 03 80 52 4A ?? ?? F9 4A ?? ?? F8", -36},
-        };
+      uintptr_t GetGUObjectArrayPtr() const override
+      {
+          std::vector<std::pair<std::string, int>> idaPatterns = {
+              // ADRP X25, #off_DDBBA68@PAGE / LDR X25, [X25, #PAGEOFF] @ 0x71091A4
+              {"99 65 03 D0 39 37 45 F9", 0},
+              // ADRP X0, #off_DDBBA68@PAGE / LDR X0, [X0, #PAGEOFF] @ 0x71091C4
+              {"80 65 03 D0 00 34 45 F9", 0},
+              // ADRP X9, #off_DDBBA68@PAGE / LDR X9, [X9, #PAGEOFF] @ 0x32C0E08 (with MOV between)
+              {"C9 57 05 F0 ?? ?? ?? ?? 29 35 45 F9", 0},
+              // ADRP X10, #off_DDBBA68@PAGE (common pattern: LDR W8,[Xreg,#0xC] / ADRP X10 / ADD W9,W8,W9)
+              {"48 0C 40 B9 4A 59 05 ??", 4},
+              // ADRP X9, #off_DDBBA68@PAGE (alt register variant)
+              {"48 0C 40 B9 49 59 05 ??", 4},
+          };
 
-        PATTERN_MAP_TYPE map_type = isEmulator() ? PATTERN_MAP_TYPE::ANY_R : PATTERN_MAP_TYPE::ANY_X;
+          PATTERN_MAP_TYPE map_type = isEmulator() ? PATTERN_MAP_TYPE::ANY_R : PATTERN_MAP_TYPE::ANY_X;
 
-        for (const auto &it : idaPatterns)
-        {
-            std::string ida_pattern = it.first;
-            const int step = it.second;
+          for (const auto &it : idaPatterns)
+          {
+              std::string ida_pattern = it.first;
+              const int step = it.second;
 
-            uintptr_t adrl = Arm64::DecodeADRL(findIdaPattern(map_type, ida_pattern, step));
-            if (adrl != 0) return adrl;
-        }
+              uintptr_t adrl = Arm64::DecodeADRL(findIdaPattern(map_type, ida_pattern, step));
+              if (adrl != 0) return adrl;
+          }
 
-        return 0;
-    }
+          return 0;
+      }
 
-    uintptr_t GetNamesPtr() const override
-    {
-        std::vector<std::pair<std::string, int>> idaPatterns = {
-            // sub_6341A70: STP / ADRP X8 / LDR X21 / CBZ / ADD X20,X21,#0x20
-            {"F4 4F 02 A9 28 ?? ?? B0 15 ?? ?? F9 75 06 00 B4 B4 82 00 91", 4},
-            // sub_63438A8 / sub_6343D0C: MRS / ADRP X10 / LDR X9 / MOV X19,X8 / STR
-            {"56 D0 3B D5 0A ?? ?? F0 C9 16 40 F9 F3 03 08 AA", 4},
-            // sub_6348E18: STP / ADRP X21 / LDR X8 / CBZ
-            {"F4 4F 05 A9 F5 ?? ?? D0 A8 06 46 F9 E8 07 00 B4", 4},
-            // sub_710A8B4 (wrapper): ADRP X8 / LDR X8 / CBZ
-            {"E8 ?? ?? 90 08 ?? ?? F9 A8 00 00 B4", 0},
-            // sub_710A8B4 full: RET / ADRP X8 / LDR X8 / CBZ
-            {"C0 03 5F D6 E8 ?? ?? 90 08 ?? ?? F9 A8 00 00 B4", 4},
-            // 0x712861c: MRS / ADRP X27 / LDR X8 / STUR
-            {"5A D0 3B D5 FB ?? ?? D0 48 17 40 F9 A8 83 1F F8", 4},
-            // sub_7FD1ACC: BLR / ADRP X22 / LDR X0 / CBZ / ADD
-            {"20 01 3F D6 B6 ?? ?? B0 C0 ?? ?? F9 80 00 00 B4 E2 23 00 91", 4},
-        };
+      uintptr_t GetNamesPtr() const override
+      {
+          std::vector<std::pair<std::string, int>> idaPatterns = {
+              // ADRL X8, unk_E427F40 @ 0x58EBBC8 (back-to-back ADRP+ADD, direct struct)
+              {"E8 59 04 90 08 01 3D 91", 0},
+              // ADRP X0, #unk_E427F40@PAGE / MOV W20,W2 / ADD X0,X0,#PAGEOFF @ 0x56E95D4
+              {"E0 69 04 D0 ?? ?? ?? ?? 00 00 3D 91", 0},
+              // ADRP X8, #unk_E427F40@PAGE / ADD X8,X8,#PAGEOFF (wildcarded reg)
+              {"?? 59 04 90 ?? 01 3D 91", 0},
+              // ADRP X20, #unk_E427F40@PAGE / ADD X20,X20,#PAGEOFF @ 0x6318830
+              {"74 08 04 F0 ?? ?? ?? ?? 94 02 3D 91", 0},
+              // ADRP X21, #unk_E427F40@PAGE / MOV X19,X1 / MOV X20,X0 / ADD X21,X21,#PAGEOFF @ 0x6939B38
+              {"75 D7 03 D0 F3 03 01 AA F4 03 00 AA B5 02 3D 91", 0},
+              // ADRP X21, #unk_E427F40@PAGE / MOV X19,X1 / MOV X20,X0 @ 0x572ADE8 (ADD at +16)
+              {"F5 67 04 B0 F3 03 01 AA F4 03 00 AA", 0},
+              // ADRP X10, #unk_E427F40@PAGE / ADD X10,X10,#PAGEOFF @ 0x5B155E4 (ADRL)
+              {"8A 48 04 D0 4A 01 3D 91", 0},
+          };
 
-        PATTERN_MAP_TYPE map_type = isEmulator() ? PATTERN_MAP_TYPE::ANY_R : PATTERN_MAP_TYPE::ANY_X;
+          PATTERN_MAP_TYPE map_type = isEmulator() ? PATTERN_MAP_TYPE::ANY_R : PATTERN_MAP_TYPE::ANY_X;
 
-        for (const auto &it : idaPatterns)
-        {
-            std::string ida_pattern = it.first;
-            const int step = it.second;
+          for (const auto &it : idaPatterns)
+          {
+              std::string ida_pattern = it.first;
+              const int step = it.second;
 
-            uintptr_t adrl = Arm64::DecodeADRL(findIdaPattern(map_type, ida_pattern, step));
-            if (adrl != 0) return adrl;
-        }
+              uintptr_t adrl = Arm64::DecodeADRL(findIdaPattern(map_type, ida_pattern, step));
+              if (adrl != 0) return adrl;
+          }
 
-        return 0;
-    }
+          return 0;
+      }
 
-    UE_Offsets *GetOffsets() const override
-    {
-        static UE_Offsets offsets = UE_DefaultOffsets::UE4_25_27(isUsingCasePreservingName());
+      UE_Offsets *GetOffsets() const override
+      {
+          static UE_Offsets offsets = UE_DefaultOffsets::UE4_25_27(isUsingCasePreservingName());
 
-        static bool once = false;
-        if (!once)
-        {
-            once = true;
-            offsets.FNamePool.BlocksOff += sizeof(void *);
+          static bool once = false;
+          if (!once)
+          {
+              once = true;
+              offsets.FNamePool.BlocksOff += sizeof(void *);
 
-            // https://github.com/MJx0/AndUEDumper/issues/42
-            offsets.UStruct.SuperStruct += sizeof(void *);
-            offsets.UStruct.Children += sizeof(void *);
-            offsets.UStruct.ChildProperties += sizeof(void *);
-            offsets.UStruct.PropertiesSize += sizeof(void *);
+              // https://github.com/MJx0/AndUEDumper/issues/42
+              offsets.UStruct.SuperStruct += sizeof(void *);
+              offsets.UStruct.Children += sizeof(void *);
+              offsets.UStruct.ChildProperties += sizeof(void *);
+              offsets.UStruct.PropertiesSize += sizeof(void *);
 
-            offsets.UField.Next += sizeof(void *);
-            offsets.UEnum.Names += sizeof(void *);
+              offsets.UField.Next += sizeof(void *);
+              offsets.UEnum.Names += sizeof(void *);
 
-            offsets.UFunction.EFunctionFlags += sizeof(void *);
-            offsets.UFunction.NumParams += sizeof(void *);
-            offsets.UFunction.ParamSize += sizeof(void *);
-            offsets.UFunction.Func += sizeof(void *);
-        }
+              offsets.UFunction.EFunctionFlags += sizeof(void *);
+              offsets.UFunction.NumParams += sizeof(void *);
+              offsets.UFunction.ParamSize += sizeof(void *);
+              offsets.UFunction.Func += sizeof(void *);
+          }
 
-        return &offsets;
-    }
-};
+          return &offsets;
+      }
+  };
